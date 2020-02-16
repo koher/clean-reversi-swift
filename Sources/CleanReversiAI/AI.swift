2a2fedb129925d946b4dc082c575da9ad3189240
Yuta Koshizawa <koher@koherent.org>
2020-02-16 12:27:38 +0900

    Introduce `Canceller` to "AI"

--- Sources/CleanReversiAI/AI.swift
+++ Sources/CleanReversiAI/AI.swift
@@ -1,9 +1,13 @@
 import CleanReversi
+import CleanReversiAsync
 import Dispatch
 
-public func coordinateToPlaceDisk(onto board: Board, handler: @escaping (Int, Int) -> Void) {
+public func coordinateToPlaceDisk(onto board: Board, handler: @escaping (Int, Int) -> Void) -> Canceller {
+    let canceller = Canceller {}
     DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+        if canceller.isCancelled { return }
         let (x, y) = board.validMoves(for: .dark).randomElement()!
         handler(x, y)
     }
+    return canceller
 }
