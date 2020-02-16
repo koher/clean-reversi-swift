515559046f18951fb743d1ac7e9204dd363fe87e
Yuta Koshizawa <koher@koherent.org>
2020-02-16 12:23:06 +0900

    Generate LinuxMain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,10 +1,12 @@
 import XCTest
 
 import CleanReversiAppTests
+import CleanReversiAsyncTests
 import CleanReversiTests
 
 var tests = [XCTestCaseEntry]()
 tests += CleanReversiAppTests.__allTests()
+tests += CleanReversiAsyncTests.__allTests()
 tests += CleanReversiTests.__allTests()
 
 XCTMain(tests)
