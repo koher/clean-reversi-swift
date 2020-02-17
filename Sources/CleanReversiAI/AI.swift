1e42732ac15b634dd6f4735a48ff9f5df86674da
Yuta Koshizawa <koher@koherent.org>
2020-02-17 16:57:23 +0900

    Rename "AI"'s API and add `side` to its arguments

--- Sources/CleanReversiAI/AI.swift
+++ Sources/CleanReversiAI/AI.swift
@@ -2,11 +2,11 @@ import CleanReversi
 import CleanReversiAsync
 import Dispatch
 
-public func coordinateToPlaceDisk(onto board: Board, handler: @escaping (Int, Int) -> Void) -> Canceller {
+public func move(for board: Board, of side: Disk, handler: @escaping (Int, Int) -> Void) -> Canceller {
     let canceller = Canceller {}
     DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
         if canceller.isCancelled { return }
-        let (x, y) = board.validMoves(for: .dark).randomElement()!
+        let (x, y) = board.validMoves(for: side).randomElement()!
         handler(x, y)
     }
     return canceller
